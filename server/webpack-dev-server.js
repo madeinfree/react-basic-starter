11bfd7b52e262806512cc8eb840b22059a7a370a
Whien <sal95610@gmail.com>
2017-08-15 14:30:23 +0800

    fix eslint

--- server/webpack-dev-server.js
+++ server/webpack-dev-server.js
@@ -1,24 +1,4 @@
 /* eslint no-console: 0 */
-// import webpack from 'webpack';
-// import WebpackDevServer from 'webpack-dev-server';
-// import config from '../webpack/webpack.dev.config';
-// require('dotenv').config();
-//
-// new WebpackDevServer(webpack(config), {
-//   publicPath: config.output.publicPath,
-//   contentBase: './develop',
-//   hot: true,
-//   historyApiFallback: true
-// }).listen(process.env.APP_PORT, err => {
-//   if (err) {
-//     return console.error(err);
-//   }
-//
-//   console.log(`listening at http://localhost:${process.env.APP_PORT}`);
-//
-//   return false;
-// });
-/* eslint no-console: 0 */
 /* eslint global-require: 0 */
 /* eslint no-unused-vars: 0 */
 import React from 'react';
